b978da8b8e129a134a91d23f67c1426e0d0b0ac4
fardosa-ibrahim <fardosaibrahimshide@gmail.com>
2022-02-12 08:28:03 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    var name="fardosa ibrahim"
+    println(name)
+    var age=22
+    println(age)
+    var phoneNumber="0743562256"
+    println(phoneNumber)
+    var weight=50
+    println(weight)
+    var kenyan=true
+    var notKenyan=false
+    println(kenyan)
+    println(notKenyan)
+}
